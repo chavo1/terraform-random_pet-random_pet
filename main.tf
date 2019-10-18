b5b446042cc1e269b254c0e0110a75d9ea087007
chavo1 <42143239+chavo1@users.noreply.github.com>
2019-10-18 10:39:09 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,3 +17,9 @@ resource "null_resource" "env1" {
     command = "echo $ATLAS_CONFIGURATION_VERSION_GITHUB_COMMIT_SHA"
   }
 }
+
+resource "null_resource" "env2" {
+  provisioner "local-exec" {
+    command = "ls -la"
+  }
+}
